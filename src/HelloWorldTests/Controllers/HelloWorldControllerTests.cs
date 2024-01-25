12ce2cf6e5e7e428c894b61549a098d947fb86e8
jarvars <soyjarvars@gmail.com>
2024-01-25 11:03:07 -0500

    Upload hello world example

---
+++ src/HelloWorldTests/Controllers/HelloWorldControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace HelloWorld.Controllers.Tests
+{
+    [TestFixture()]
+    public class HelloWorldControllerTests
+    {
+        [Test()]
+        public void GetTest()
+        {
+            var api = new HelloWorldController();
+            var result = api.Get();
+
+            Assert.That(result, Is.EqualTo("Hello World"));
+        }
+    }
+}
